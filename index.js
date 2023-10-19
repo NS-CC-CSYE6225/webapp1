1f11fdc75faaee8748f066349509f4b16698d7d1
Aravind Sankar <114179522+aravindsankars@users.noreply.github.com>
2023-10-19 03:32:31 -0400

    wf6 commit

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
-const readCSV = require("./services/userService");
-// const bootstrapDatabase = require("./config/databaseConfig");
-const express = require("express");
-const config = require("./config/config");
-const router = require("./routes/assignmentsRoute");
-const db = require("./models/index");
-// const { route } = require("./routes/assignmentsRoute");
+const app = require("./main");
 
-const app = express();
-
-// const { Sequelize } = require('sequelize');
-// const sequelize = new Sequelize(config);
-// const mysql2 = require('mysql2/promise');
+const { readCSV } = require("./services/userService");
 
 
-
-// Express route to check the database connection
-app.get('/healthz', (req, res) => {
-  db.sequelize
-  .authenticate()
-  .then(() => {
-    res.status(200).send();
-  })
-  .catch(() => {
-    res.status(503).send();
-  })
+readCSV().then(() => {
+  app.listen(8080, () => {
+    console.log(`Server is running on port ${8080}`);
+})
+}).catch((error) =>{
+  console.error('Error synchronizing database:', error);
 });
-
-
-app.use('/v1/assignments', router);
-
-// app.get('/v1/assignments', router);
-
-// app.put('/v1/assignments/:id', router);
-
-// app.delete('/v1/assignments/:id', router);
-
-// Start the Express server
-app.listen(8080, () => {
-  console.log(`Server is running on port ${8080}`);
-});
-
-// bootstrapDatabase;
-readCSV;
-
-module.exports = app;
-
 
---
+++ routes/healthzRoute.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const db = require("../models")
+
+// Express route to check the database connection
+router.get('/healthz', (req, res) => {
+    db.sequelize
+    .authenticate()
+    .then(() => {
+      res.status(200).send();
+    })
+    .catch(() => {
+      res.status(503).send();
+    })
+  });
+
+module.exports = router;
