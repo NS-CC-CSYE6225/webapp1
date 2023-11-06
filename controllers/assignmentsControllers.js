d41472ad1249b099a1198c3c1663eb5221b04fc5
Aravind Sankar <114179522+aravindsankars@users.noreply.github.com>
2023-11-06 18:45:32 -0500

    few changes

--- controllers/assignmentsControllers.js
+++ controllers/assignmentsControllers.js
@@ -1,176 +1,129 @@
 const { findId, findAssignmentInfo, updateAssignment, deleteAssignmentById } = require("../services/assignmentsService");
 const { Assignment } = require('../models/assignments');
 
-    // Create a new assignment
-    // router.post('/v1/assignments', authenticateUser, async (req, res) => {
-      async function createAssignment(req, res){
-        const userToken = req.headers.authorization;
-        const fields = Buffer.from(userToken.split(' ')[1], 'base64').toString().split(':');
-        const username = fields[0];
-        findId(username).then((id) => {
-          const { name, points, num_of_attemps, deadline} = req.body;
-          const assignment =  Assignment.create({
-            name: name,
-            points: points,
-            num_of_attemps: num_of_attemps,
-            deadline: deadline,
-            user_id: id
-          }).then((newAssignment) => {
-            const response = {
-              "id": newAssignment.id,
-              "name": name,
-              "points": points,
-              "num_of_attemps": num_of_attemps,
-              "deadline": deadline,
-              "assignment_created": newAssignment.assignment_created,
-              "assignment_updated": newAssignment.assignment_updated
-            };
-      
-            return res.status(201).json(response).send();
-          });
-        })
+
+async function createAssignment(req, res) {
+  const userToken = req.headers.authorization;
+  const fields = Buffer.from(userToken.split(' ')[1], 'base64').toString().split(':');
+  const username = fields[0];
+  findId(username).then((id) => {
+    const { name, points, num_of_attemps, deadline } = req.body;
+    const assignment = Assignment.create({
+      name: name,
+      points: points,
+      num_of_attemps: num_of_attemps,
+      deadline: deadline,
+      user_id: id
+    }).then((newAssignment) => {
+      const response = {
+        "id": newAssignment.id,
+        "name": name,
+        "points": points,
+        "num_of_attemps": num_of_attemps,
+        "deadline": deadline,
+        "assignment_created": newAssignment.assignment_created,
+        "assignment_updated": newAssignment.assignment_updated
+      };
+
+      return res.status(201).json(response).send();
+    });
+  })
+}
+
+async function getAllAssignments(req, res) {
+  Assignment.findAll()
+    .then((assignments) => {
+      let response = [];
+      for (let i = 0; i < assignments.length; i++) {
+        const assignment = {
+          "id": assignments[i].id,
+          "name": assignments[i].name,
+          "points": assignments[i].points,
+          "num_of_attemps": assignments[i].num_of_attemps,
+          "deadline": assignments[i].deadline,
+          "assignment_created": assignments[i].assignment_created,
+          "assignment_updated": assignments[i].assignment_updated
+        }
+        response.push(assignment);
       }
-      // });
-  
-  
-  // GET all assignments
-  // router.get('/v1/assignments', async (req, res) => {
-    async function getAllAssignments(req, res){
-      Assignment.findAll()
-      .then((assignments) =>{
-          let response = [];
-          for(let i=0; i<assignments.length; i++){
-            const assignment = {
-              "id": assignments[i].id,
-              "name": assignments[i].name,
-              "points": assignments[i].points,
-              "num_of_attemps": assignments[i].num_of_attemps,
-              "deadline": assignments[i].deadline,
-              "assignment_created": assignments[i].assignment_created,
-              "assignment_updated": assignments[i].assignment_updated
-            }
-            response.push(assignment);
-          }
-          return res.status(200).json(response).send();
-      })
+      return res.status(200).json(response).send();
+    })
+}
+
+async function getAssignment(req, res) {
+  const assignmentId = req.params.id;
+  findAssignmentInfo(assignmentId).then((assignment) => {
+    let response = [
+      {
+        "id": assignment.id,
+        "name": assignment.name,
+        "points": assignment.points,
+        "num_of_attemps": assignment.num_of_attemps,
+        "deadline": assignment.deadline,
+        "assignment_created": assignment.assignment_created,
+        "assignment_updated": assignment.assignment_updated
+      }
+    ]
+    return res.status(200).json(response).send();
+  })
+}
+
+
+async function updateAssignments(req, res) {
+  try {
+    const assignment_Id = req.params.id;
+    const userToken = req.headers.authorization;
+    const fields = Buffer.from(userToken.split(' ')[1], 'base64').toString().split(':');
+    const username = fields[0];
+    const id = await findId(username);
+
+    const assignment = await findAssignmentInfo(assignment_Id);
+
+    if (id === assignment.user_id) {
+      const { name, points, num_of_attemps, deadline } = req.body;
+      const updatedData = {
+        name: name,
+        points: points,
+        num_of_attemps: num_of_attemps,
+        deadline: deadline
+      };
+
+      const updatedAssignment = await updateAssignment(assignment_Id, updatedData);
+
+      if (updatedAssignment) {
+        return res.status(204).send();
+      } else {
+        return res.status(404).send('Assignment not found or not updated');
+      }
+    } else {
+      return res.status(401).send('Unauthorized');
     }
-  // });
-  
-  //Get particular Assignment
-  // router.get('/v1/assignments/:id', authenticateUser, (req,res) => {
-      
-  async function getAssignment(req, res){
-      const assignmentId = req.params.id;
-      findAssignmentInfo(assignmentId).then((assignment) => {
-        let response = [
-          {
-            "id": assignment.id,
-            "name": assignment.name,
-            "points": assignment.points,
-            "num_of_attemps": assignment.num_of_attemps,
-            "deadline": assignment.deadline,
-            "assignment_created": assignment.assignment_created,
-            "assignment_updated": assignment.assignment_updated
-          }
-        ]
-        return res.status(200).json(response).send();
-      })
-    }
-    // });
-  
-  
-  //Update Assignment
-  // router.put('/v1/assignments/:id', authenticateUser, (req,res) => {
-      
-  async function updateAssignments(req, res){
-      const assignment_Id = req.params.id;
-      findAssignmentInfo(assignment_Id).then((assignment) => {
-        const userToken = req.headers.authorization;
-        const fields = Buffer.from(userToken.split(' ')[1], 'base64').toString().split(':');
-        const username = fields[0];
-        findId(username).then((id) => {
-          if (id == assignment.user_id){
-            const { name, points, num_of_attemps, deadline } = req.body;
-            const updatedData = {
-              name: name,
-              points: points,
-              num_of_attemps: num_of_attemps,
-              deadline: deadline
-            };
-            updateAssignment(assignment_Id, updatedData).then(() => {
-              return res.status(204).send();
-            });
-          }
+  } catch (error) {
+    console.error('Error in updateAssignments:', error);
+    return res.status(500).send('Internal Server Error');
+  }
+}
+
+async function deleteAssignment(req, res) {
+  const assignmentId = req.params.id;
+  findAssignmentInfo(assignmentId).then((assignment) => {
+    const userToken = req.headers.authorization;
+    const fields = Buffer.from(userToken.split(' ')[1], 'base64').toString().split(':');
+    const username = fields[0];
+    findId(username).then((id) => {
+      if (id == assignment.user_id) {
+
+        deleteAssignmentById(assignmentId).then(() => {
+          return res.status(204).send();
         });
-    
-      })
-    }
+      }
+    });
+
+  })
+}
 
 
-//   async function updateAssignments(req, res) {
-//     const assignment_Id = req.params.id;
-//     const userToken = req.headers.authorization;
-//     const fields = Buffer.from(userToken.split(' ')[1], 'base64').toString().split(':');
-//     const username = fields[0];
-    
-//     try {
-//         const id = await findId(username);
-//         const assignment = await findAssignmentInfo(assignment_Id);
 
-//         console.log('ID:', id);
-//         console.log('User ID f-----rom Assignment:', assignment.user_id);
-
-
-//         if (id === assignment.user_id) {
-//             const { name, points, num_of_attemps, deadline, user_id } = req.body;
-//             const updatedData = {
-//                 name: name,
-//                 points: points,
-//                 num_of_attemps: num_of_attemps,
-//                 deadline: deadline,
-//                 user_id: user_id
-//             };
-            
-//             const updatedAssignment = await updateAssignment(assignment_Id, updatedData);
-            
-//             if (updatedAssignment) {
-//                 res.status(204).send();
-//             } else {
-//                 res.status(400).send('Assignment not found or not updated');
-//             }
-//         } else {
-//             res.status(401).send('Unauthorized');
-//         }
-//     } catch (error) {
-//         console.error('Error updating assignment:', error);
-//         res.status(500).send('Internal Server Error');
-//     }
-// }
-
-  
-    //Delete Assignment
-    // router.delete('/v1/assignments/:id', authenticateUser, (req,res) => {
-      
-    async function deleteAssignment(req, res){  
-      const assignmentId = req.params.id;
-      findAssignmentInfo(assignmentId).then((assignment) => {
-        const userToken = req.headers.authorization;
-        const fields = Buffer.from(userToken.split(' ')[1], 'base64').toString().split(':');
-        const username = fields[0];
-        findId(username).then((id) => {
-          if (id == assignment.user_id){
-            
-            deleteAssignmentById(assignmentId).then(() => {
-              return res.status(204).send();
-            });
-          }
-        });
-    
-      })
-    }
-    // });
-  
-  
-    module.exports = {
-      createAssignment, getAllAssignments, getAssignment, updateAssignments, deleteAssignment
-    };
+module.exports = {
+  createAssignment, getAllAssignments, getAssignment, updateAssignments, deleteAssignment
+};
--- services/assignmentsService.js
+++ services/assignmentsService.js
@@ -1,101 +1,93 @@
 const { User } = require('../models/user');
 const { Assignment } = require('../models/assignments');
-// const db = require('../models');
+
+async function findId(pEmail) {
+  const aUser = await User.findOne({
+    where: {
+      email: pEmail,
+    },
+  });
+  const Id = String(aUser.id);
+  return Id;
+}
+
+async function findPwd(pEmail) {
+  console.log("email is ->" + pEmail);
+  console.log("User is ->" + User);
+  const aUser = await User.findOne({
+    where: {
+      email: pEmail,
+    },
+  });
+  const Pwd = String(aUser.password);
+  console.log(Pwd);
+  return Pwd;
+}
 
 
-async function findId(pEmail){
-    const aUser = await User.findOne({
+async function findAssignmentInfo(assignmentId) {
+  try {
+    const assignmentInfo = await Assignment.findOne({
       where: {
-        email: pEmail,
+        id: assignmentId,
       },
     });
-    const Id = String(aUser.id); 
-    return Id;
-  }
 
-async function findPwd(pEmail){
-console.log("email is ->" + pEmail);
-console.log("User is ->" + User);
-    const aUser = await User.findOne({
-      where: {
-        email: pEmail,
-      },
+    return assignmentInfo;
+  } catch (error) {
+    console.error('Error finding assignment information:', error);
+    throw error;
+  }
+}
+
+async function updateAssignment(assignmentId, updatedData) {
+
+  try {
+
+    const [affectedRows, [updatedAssignment]] = await Assignment.update(updatedData, {
+      where: { id: assignmentId },
+      returning: true,
     });
-    const Pwd = String(aUser.password);
-    console.log(Pwd);
-    return Pwd;
-  }
+    console.log("updated", updatedAssignment);
 
-
-  async function findAssignmentInfo(assignmentId) {
-    try {
-        const assignmentInfo = await Assignment.findOne({
-            where: {
-                id: assignmentId,
-            },
-        });
-  
-        return assignmentInfo;
-    } catch (error) {
-        console.error('Error finding assignment information:', error);
-        throw error;
-    }
-  }
-  
-  async function updateAssignment(assignmentId, updatedData) {
-
-    // console.log("updated", updatedData);
-
-    try {
-      
-        const [affectedRows, [updatedAssignment]] = await Assignment.update(updatedData, {
-            where: { id: assignmentId },
-            returning: true, 
-        });
-        console.log("updated", updatedAssignment);
-        // if (affectedRows > 0) {
-        //     console.log('Assignment updated successfully:', updatedAssignment);
-        //     return updatedAssignment;
-        // } 
-
-        if (affectedRows > 0) {
-          if (Array.isArray(updatedAssignment)) {
-              const updatedAssignment = updatedAssignment[0];
-              console.log('Assignment updated successfully:', updatedAssignment);
-              return updatedAssignment;
-          }
+    if (affectedRows > 0) {
+      if (Array.isArray(updatedAssignment)) {
+        const updatedAssignment = updatedAssignment[0];
+        console.log('Assignment updated successfully:', updatedAssignment);
+        return updatedAssignment;
       }
-
-        else {
-            console.log('Assignment not found or not updated');
-            return null;
-        }
-    } catch (error) {
-        console.error('Error updating assignment:', error);
-        throw error; 
     }
-  }
-  
-  
-  async function deleteAssignmentById(assignmentId) {
-    try {
-        const deletedRows = await Assignment.destroy({
-            where: { id: assignmentId },
-        });
-  
-        if (deletedRows > 0) {
-            console.log('Assignment deleted successfully');
-            return true; 
-        } else {
-            console.log('Assignment not found or not deleted');
-            return false; 
-        }
-    } catch (error) {
-        console.error('Error deleting assignment:', error);
-        throw error; 
-    }
-  }
 
-  module.exports = {
-    findId, findPwd, findAssignmentInfo, updateAssignment, deleteAssignmentById
-  };
+    else if (affectedRows === 0) {
+      console.log('Assignment not found or not updated');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error updating assignment:', error);
+    throw error;
+  }
+}
+
+
+async function deleteAssignmentById(assignmentId) {
+  try {
+    const deletedRows = await Assignment.destroy({
+      where: { id: assignmentId },
+    });
+
+    if (deletedRows > 0) {
+      console.log('Assignment deleted successfully');
+      return true;
+    } else {
+      console.log('Assignment not found or not deleted');
+      return false;
+    }
+  } catch (error) {
+    console.error('Error deleting assignment:', error);
+    throw error;
+  }
+}
+
+module.exports = {
+  findId, findPwd, findAssignmentInfo, updateAssignment, deleteAssignmentById
+};
