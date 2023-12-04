5bf3bba26286ffdc6b7ef41049272371b922377d
Aravind Sankar <114179522+aravindsankars@users.noreply.github.com>
2023-12-04 17:02:03 -0500

    Assignment10

--- main.js
+++ main.js
@@ -4,12 +4,27 @@ const app = express();
 const assignmentRouter = require("./routes/assignmentsRoute");
 const healthzRouter = require("./routes/healthzRoute");
 
-app.use('/v1/assignments', assignmentRouter);
+app.use('/v2/assignments', assignmentRouter);
 
 app.use('/healthz', healthzRouter);
 
-app.use('/', (req, res) => {
-    res.status(200).send(); // Sending 'OK' with a 200 status code
-  });
+// app.use('/', (req, res) => {
+//     res.status(200).send(); // Sending 'OK' with a 200 status code
+//   });
+
+app.use('/', (req, res , next) => {
+  if(req.path === '/'){
+      res.status(200).end();
+
+  }else{
+      next();
+  }
+});
+
+// Handle all other routes with a 404 response
+app.use('*', (req, res) => {
+    res.status(404).end();
+});
+
 
 module.exports = app
