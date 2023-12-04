d322927888d1f7004a67433ab5a5a6b285efe1f3
Aravind Sankar <114179522+aravindsankars@users.noreply.github.com>
2023-12-04 17:21:29 -0500

    Assignment10

--- main.js
+++ main.js
@@ -8,8 +8,22 @@ app.use('/v1/assignments', assignmentRouter);
 
 app.use('/healthz', healthzRouter);
 
-app.use('/', (req, res) => {
-    res.status(200).send(); // Sending 'OK' with a 200 status code
-  });
+// app.use('/', (req, res) => {
+//     res.status(200).send(); // Sending 'OK' with a 200 status code
+//   });
+
+app.use('/', (req, res , next) => {
+  if(req.path === '/'){
+      res.status(200).end();
+
+  }else{
+      next();
+  }
+});
+
+// Handle all other routes with a 404 response
+app.use('*', (req, res) => {
+    res.status(404).end();
+});
 
 module.exports = app
